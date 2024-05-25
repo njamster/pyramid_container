c3ac24e29a0b5531be1664a360d23812aaf14967
njamster <njamster@users.noreply.github.com>
2024-05-25 17:44:53 +0200

    Add custom icon

--- addons/pyramid_container/plugin.gd
+++ addons/pyramid_container/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("PyramidContainer", "Container", preload("pyramid_container.gd"), preload("icon.svg"))
+	add_custom_type("PyramidContainer", "Container", preload("pyramid_container.gd"), preload("res://icon.svg"))
 
 
 func _exit_tree() -> void:
