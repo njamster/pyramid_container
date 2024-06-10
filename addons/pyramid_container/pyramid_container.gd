1c5cc72ccbc66ae7fbd988af7b25b4e50ceb2257
njamster <njamster@users.noreply.github.com>
2024-06-10 23:28:14 +0200

    Implement proper container resizing
    This includes:
    - calculating the minimum size of the container based on its content
    - stretching the size of elements based on the container's size flags
    - limiting the available size flags for children in the editor
    
    Note: The 'direction' property is broken for now, will fix this later!

--- addons/pyramid_container/pyramid_container.gd
+++ addons/pyramid_container/pyramid_container.gd
@@ -13,104 +13,198 @@ enum Direction {
 @export var direction := Direction.UP:
 	set(value):
 		direction = value
-		if is_inside_tree():
-			queue_sort()
+		queue_sort()
 
 
 @export_group("Line Drawing", "draw_")
 @export var draw_enabled := false:
 	set(value):
 		draw_enabled = value
-		if is_inside_tree():
-			queue_redraw()
+		queue_redraw()
 
 @export_range(1, 10, 1, "or_greater", "suffix:px") var draw_width := 3.0:
 	set(value):
 		draw_width = value
-		if is_inside_tree():
-			queue_redraw()
+		queue_redraw()
 
 @export_range(0, 50, 1, "or_greater", "suffix:px") var draw_shortened := 10:
 	set(value):
 		draw_shortened = value
-		if is_inside_tree():
-			queue_redraw()
+		queue_redraw()
 
 @export var draw_color := Color.WHITE:
 	set(value):
 		draw_color = value
-		if is_inside_tree():
-			queue_redraw()
+		queue_redraw()
 
 @export var draw_antialiased := true:
 	set(value):
 		draw_antialiased = value
-		if is_inside_tree():
-			queue_redraw()
+		queue_redraw()
 
 
-# for internal use only!
-var _num_layers : int
-var _num_children : int
-var _biggest_included_power_of_two : int
+# internal variables
+var _layer_width : Array[float] = [ 0.0 ]
+var _node_heights : Array[float] = []
+
+
+func _highest_included_power_of_two(n: int) -> int:
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	return n ^ (n >> 1)
+
+
+func _get_minimum_size() -> Vector2:
+	var minimum_size := Vector2.ZERO
+
+	# only count child nodes that are currently visible
+	var sortable_children : Array[Control] = []
+	for child in get_children():
+		if child is Control and child.visible:
+			sortable_children.append(child)
+
+	var sortable_child_count := sortable_children.size()
+
+	# if there are none, we're done here: there is no minimum size!
+	if sortable_child_count == 0:
+		return minimum_size
+
+	# reset internal helper variables
+	_node_heights = []
+	_layer_width = [ 0.0 ]
+
+	var layer := 0
+	var pos_in_layer := 0
+	var max_nodes_in_layer := _highest_included_power_of_two(sortable_child_count)
+
+	for i in sortable_child_count:
+		var child = sortable_children[i]
+
+		var child_size := child.get_combined_minimum_size()
+		if layer > 0:
+			# child should be at least as high as its predecessors from the previous layer combined
+			var predecessor_1_height := _node_heights[i - 2 * max_nodes_in_layer]
+			var predecessor_2_height := _node_heights[i - 2 * max_nodes_in_layer + 1]
+			child_size.y = max(child_size.y, predecessor_1_height + predecessor_2_height)
+
+		_layer_width[layer] = max(_layer_width[layer], child_size.x)
+
+		_node_heights.append(child_size.y)
+		# if child has no successor in the next layer...
+		if i + max_nodes_in_layer - ceil(0.5 * pos_in_layer) >= sortable_child_count:
+			# ...its size is part of the minimum size of the container
+			minimum_size.y += child_size.y
+
+		if pos_in_layer == max_nodes_in_layer -1 or i == sortable_child_count - 1:
+			minimum_size.x += _layer_width[layer]
+
+		if pos_in_layer == max_nodes_in_layer -1:
+			layer += 1
+			pos_in_layer = 0
+			max_nodes_in_layer /= 2
+			if i < sortable_child_count - 1:
+				_layer_width.append(0.0)
+		else:
+			pos_in_layer += 1
+
+	return minimum_size
 
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_SORT_CHILDREN:
 		_resort()
-		queue_redraw()
 
 
 func _resort() -> void:
-	_num_children = get_child_count()
+	# only count child nodes that are currently visible
+	var sortable_children : Array[Control] = []
+	for child in get_children():
+		if child is Control and child.visible:
+			sortable_children.append(child)
 
-	_num_layers = 0
-	var remaining_children := _num_children
-	_biggest_included_power_of_two = int(log(_num_children) / log(2))
-	while remaining_children > 0:
-		remaining_children -= 2 ** (_biggest_included_power_of_two - _num_layers)
-		_num_layers += 1
+	var sortable_child_count := sortable_children.size()
+
+	# if there are none, we're done here: there is nothing to sort!
+	if sortable_child_count == 0:
+		return
+
+	var stretch_space := size - _get_minimum_size()
 
 	var layer := 0
+	var offset := Vector2.ZERO
 	var pos_in_layer := 0
-	var num_nodes_in_layer := 2 ** _biggest_included_power_of_two
+	var max_nodes_in_layer := _highest_included_power_of_two(sortable_child_count)
 
-	for i in range(_num_children):
-		var factor := layer
-		if direction == Direction.LEFT or direction == Direction.UP:
-			factor = _num_layers - layer - 1
+	for i in sortable_child_count:
+		var child := sortable_children[i]
 
-		match direction:
-			# horizontal directions
-			Direction.LEFT, Direction.RIGHT:
-				var item_size := Vector2(size.x / float(_num_layers), size.y / float(num_nodes_in_layer))
-				fit_child_in_rect(get_child(i), Rect2(item_size * Vector2(factor, pos_in_layer), item_size))
-			# vertical directions
-			Direction.UP, Direction.DOWN:
-				var item_size := Vector2(size.x / float(num_nodes_in_layer), size.y / float(_num_layers))
-				fit_child_in_rect(get_child(i), Rect2(item_size * Vector2(pos_in_layer, factor), item_size))
+		var width = _layer_width[layer]
+		if size_flags_horizontal & SIZE_FILL:
+			width += stretch_space.x / _layer_width.size()
 
-		if pos_in_layer == num_nodes_in_layer - 1:
+		var height = _node_heights[i]
+		if size_flags_vertical & SIZE_FILL:
+			height += stretch_space.y / max_nodes_in_layer
+
+		fit_child_in_rect(child, Rect2(offset.x, offset.y, width, height))
+
+		if pos_in_layer == max_nodes_in_layer - 1  or i == sortable_child_count - 1:
+			offset = Vector2(offset.x + width, 0)
 			layer += 1
 			pos_in_layer = 0
-			num_nodes_in_layer /= 2
+			max_nodes_in_layer /= 2
 		else:
+			offset.y += height
 			pos_in_layer += 1
+
+	queue_redraw()
+
+
+func _get_allowed_size_flags_horizontal() -> PackedInt32Array:
+	return [
+		SIZE_FILL,
+		SIZE_SHRINK_BEGIN,
+		SIZE_SHRINK_CENTER,
+		SIZE_SHRINK_END
+	]
+
+
+func _get_allowed_size_flags_vertical() -> PackedInt32Array:
+	return [
+		SIZE_FILL,
+		SIZE_SHRINK_BEGIN,
+		SIZE_SHRINK_CENTER,
+		SIZE_SHRINK_END
+	]
 
 
 func _draw() -> void:
 	if not draw_enabled:
 		return
 
-	var num_nodes_in_first_layer := 2 ** _biggest_included_power_of_two
+	var sortable_children : Array[Control] = []
+	for child in get_children():
+		if child is Control and child.visible:
+			sortable_children.append(child)
 
-	for i in range(_num_children):
+	var sortable_child_count := sortable_children.size()
+
+	# if there are none, we're done here: there is nothing to draw
+	if sortable_child_count == 0:
+		return
+
+	var num_nodes_in_first_layer := _highest_included_power_of_two(sortable_child_count)
+
+	for i in range(sortable_child_count):
 		var successor_id := num_nodes_in_first_layer + i / 2
-		if successor_id >= _num_children:
+		if successor_id >= sortable_child_count:
 			continue
 
-		var node_a = get_child(i)
-		var node_b = get_child(num_nodes_in_first_layer + i / 2)
+		var node_a = sortable_children[i]
+		var node_b = sortable_children[num_nodes_in_first_layer + i / 2]
 
 		if not (node_a.visible and node_b.visible):
 			continue
